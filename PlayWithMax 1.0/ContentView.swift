720a5c400f952dc7d4840e9c46ef0d9414711ac1
Lucie Hrbkova <lucie.hrbkova@xdesign.com>
2024-07-25 21:31:12 +0100

    Initial Commit

---
+++ PlayWithMax 1.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlayWithMax 1.0
+//
+//  Created by Lucie Hrbkova on 25/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
