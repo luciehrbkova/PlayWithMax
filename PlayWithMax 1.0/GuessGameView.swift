626edc868ab6adadc1617e8fb9c42882e23fc716
luciehrbkova <hrbkova.lucka@gmail.com>
2024-07-26 15:16:18 +0100

    GuessGameView created

---
+++ PlayWithMax 1.0/GuessGameView.swift
@@ -0,0 +1,18 @@
+//
+//  GuessGameView.swift
+//  PlayWithMax 1.0
+//
+//  Created by Lucie Hrbkova on 26/07/2024.
+//
+
+import SwiftUI
+
+struct GuessGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuessGameView()
+}
